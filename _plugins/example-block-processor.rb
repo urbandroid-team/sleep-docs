134f1fbbfd23f9b473543880f308ba55843a869e
Jiří Richter <richtej2@gmail.com>
2020-01-15 14:29:06 +0100

    proofreading + example block

---
+++ _plugins/example-block-processor.rb
@@ -0,0 +1,38 @@
+# Usage:
+#
+# [EXAMPLE]
+# Alarm time: 8:00
+# Smart period: 30 minutes (default)
+# Sleep as Android will wake you between 7:30-8:00, once you are in light sleep for some time. The scheduled alarm time (8:00) is considered as the last possible wake up time.
+#
+#
+
+require 'asciidoctor'
+require 'asciidoctor/extensions'
+
+class ExampleBlock < Asciidoctor::Extensions::BlockProcessor
+  use_dsl
+
+  named :EXAMPLE
+  on_context :paragraph
+  parse_content_as :simple
+
+  def process parent, reader, attrs
+    text = (reader.lines.join("<br>"))
+
+    html = %(<div class="admonitionblock example">
+    <table>
+    <tbody><tr>
+    <td class="icon">
+    <img src="/assets/icons/example.svg" alt="Example">
+    </td>
+    <td class="content">
+    #{text}
+    </td>
+    </tr>
+    </tbody></table>
+    </div>)
+
+    create_pass_block parent, html, attrs
+  end
+end
--- docs/sleep_advanced/sleep_advanced.md
+++ docs/sleep_advanced/sleep_advanced.md
@@ -1,6 +1,6 @@
 ---
 layout: default
-title: Sleep - advanced features
+title: Advanced features
 nav_order: 2
 has_children: true
 permalink: /docs/sleep_advanced
