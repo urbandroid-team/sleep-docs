8480df10e0e27e802b01884ef91155bb5414d506
artaud <richtej2@gmail.com>
2020-06-09 11:05:23 +0200

    Remove linkByTitle macro

--- _plugins/asciidoctor-extensions.rb
+++ _plugins/asciidoctor-extensions.rb
@@ -3,5 +3,5 @@ require 'asciidoctor/extensions'
 Asciidoctor::Extensions.register do
   block_macro ExampleBlockMacro if document.basebackend? 'html'
   block ExampleBlock if document.basebackend? 'html'
-  inline_macro LinkByTitle
+#   inline_macro LinkByTitle
 end
