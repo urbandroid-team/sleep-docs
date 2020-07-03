074e35b1f9988b3c1a954155f0cde976fbbb329c
artaud <richtej2@gmail.com>
2020-07-03 08:00:11 +0200

    Test gh action

--- _plugins/example-block-macro.rb
+++ _plugins/example-block-macro.rb
@@ -1,5 +1,6 @@
 # Use like this:
 # EXAMPLE::exampleText[]
+require 'asciidoctor'
 
 class ExampleBlockMacro < Asciidoctor::Extensions::BlockMacroProcessor
   use_dsl
@@ -7,14 +8,7 @@ class ExampleBlockMacro < Asciidoctor::Extensions::BlockMacroProcessor
   named :EXAMPLE
 
   def process parent, target, attrs
-    # title_html = (attrs.has_key? 'title') ?
-        # %(<div class="title">#{attrs['title']}</div>\n) : nil
 
-    # html = %(<div class="openblock gist">
-    #  {title_html}<div class="content">
-    # <script src="https://gist.github.com/#{target}.js"></script>
-    # </div>
-    # </div>)
     html = %(<div class="admonitionblock example">
     <table>
     <tbody><tr>
